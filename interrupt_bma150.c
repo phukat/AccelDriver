5304fe33f9e840053d5ec63220082438c51ac84b
phukat <omapworld@gmail.com>
2010-03-08 13:08:39 -0600

    first commit

---
+++ interrupt_bma150.c
@@ -0,0 +1,102 @@
+/*
+* This software program is licensed subject to the GNU General Public License
+* (GPL).Version 2,June 1991, available at http://www.fsf.org/copyleft/gpl.html
+*
+*
+*/
+
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<stdlib.h>
+#include<linux/ioctl.h>
+#include<stdio.h>
+#include<signal.h>
+#include<unistd.h>
+
+#define BMA150_IOC_MAGIC 'B'
+
+#define BMA150_SOFT_RESET				_IO(BMA150_IOC_MAGIC,0)
+#define BMA150_SET_RANGE				_IOWR(BMA150_IOC_MAGIC,4, unsigned char)
+#define BMA150_GET_RANGE				_IOWR(BMA150_IOC_MAGIC,5, unsigned char)
+#define BMA150_SET_MODE					_IOWR(BMA150_IOC_MAGIC,6, unsigned char)
+#define BMA150_GET_MODE					_IOWR(BMA150_IOC_MAGIC,7, unsigned char)
+#define BMA150_SET_BANDWIDTH			_IOWR(BMA150_IOC_MAGIC,8, unsigned char)
+#define BMA150_GET_BANDWIDTH			_IOWR(BMA150_IOC_MAGIC,9, unsigned char)
+#define BMA150_SET_WAKE_UP_PAUSE		_IOWR(BMA150_IOC_MAGIC,10,unsigned char)
+#define BMA150_GET_WAKE_UP_PAUSE		_IOWR(BMA150_IOC_MAGIC,11,unsigned char)
+#define BMA150_SET_LOW_G_THRESHOLD		_IOWR(BMA150_IOC_MAGIC,12,unsigned char)
+#define BMA150_GET_LOW_G_THRESHOLD		_IOWR(BMA150_IOC_MAGIC,13,unsigned char)
+#define BMA150_SET_LOW_G_COUNTDOWN		_IOWR(BMA150_IOC_MAGIC,14,unsigned char)
+#define BMA150_GET_LOW_G_COUNTDOWN		_IOWR(BMA150_IOC_MAGIC,15,unsigned char)
+#define BMA150_SET_HIGH_G_COUNTDOWN		_IOWR(BMA150_IOC_MAGIC,16,unsigned char)
+#define BMA150_GET_HIGH_G_COUNTDOWN		_IOWR(BMA150_IOC_MAGIC,17,unsigned char)
+#define BMA150_SET_LOW_G_DURATION		_IOWR(BMA150_IOC_MAGIC,18,unsigned char)
+#define BMA150_GET_LOW_G_DURATION		_IOWR(BMA150_IOC_MAGIC,19,unsigned char)
+#define BMA150_SET_HIGH_G_THRESHOLD		_IOWR(BMA150_IOC_MAGIC,20,unsigned char)
+#define BMA150_GET_HIGH_G_THRESHOLD		_IOWR(BMA150_IOC_MAGIC,21,unsigned char)
+#define BMA150_SET_HIGH_G_DURATION		_IOWR(BMA150_IOC_MAGIC,22,unsigned char)
+#define BMA150_GET_HIGH_G_DURATION		_IOWR(BMA150_IOC_MAGIC,23,unsigned char)
+#define BMA150_SET_ANY_MOTION_THRESHOLD	_IOWR(BMA150_IOC_MAGIC,24,unsigned char)
+#define BMA150_GET_ANY_MOTION_THRESHOLD	_IOWR(BMA150_IOC_MAGIC,25,unsigned char)
+#define BMA150_SET_ANY_MOTION_COUNT		_IOWR(BMA150_IOC_MAGIC,26,unsigned char)
+#define BMA150_GET_ANY_MOTION_COUNT		_IOWR(BMA150_IOC_MAGIC,27,unsigned char)
+#define BMA150_SET_INTERRUPT_MASK		_IOWR(BMA150_IOC_MAGIC,28,unsigned char)
+#define BMA150_GET_INTERRUPT_MASK		_IOWR(BMA150_IOC_MAGIC,29,unsigned char)
+#define BMA150_RESET_INTERRUPT			_IO(BMA150_IOC_MAGIC,30)
+#define BMA150_READ_ACCEL_X				_IOWR(BMA150_IOC_MAGIC,31,short)
+#define BMA150_READ_ACCEL_Y				_IOWR(BMA150_IOC_MAGIC,32,short)
+#define BMA150_READ_ACCEL_Z				_IOWR(BMA150_IOC_MAGIC,33,short)
+#define BMA150_GET_INTERRUPT_STATUS		_IOWR(BMA150_IOC_MAGIC,34,unsigned char)
+#define BMA150_SET_LOW_G_INT			_IOWR(BMA150_IOC_MAGIC,35,unsigned char)
+#define BMA150_SET_HIGH_G_INT			_IOWR(BMA150_IOC_MAGIC,36,unsigned char)
+#define BMA150_SET_ANY_MOTION_INT		_IOWR(BMA150_IOC_MAGIC,37,unsigned char)
+#define BMA150_SET_ALERT_INT			_IOWR(BMA150_IOC_MAGIC,38,unsigned char)
+#define BMA150_SET_ADVANCED_INT			_IOWR(BMA150_IOC_MAGIC,39,unsigned char)
+#define BMA150_LATCH_INT				_IOWR(BMA150_IOC_MAGIC,40,unsigned char)
+#define BMA150_SET_NEW_DATA_INT			_IOWR(BMA150_IOC_MAGIC,41,unsigned char)
+#define BMA150_GET_LOW_G_HYST			_IOWR(BMA150_IOC_MAGIC,42,unsigned char)
+#define BMA150_SET_LOW_G_HYST			_IOWR(BMA150_IOC_MAGIC,43,unsigned char)
+#define BMA150_GET_HIGH_G_HYST			_IOWR(BMA150_IOC_MAGIC,44,unsigned char)
+#define BMA150_SET_HIGH_G_HYST			_IOWR(BMA150_IOC_MAGIC,45,unsigned char)
+#define BMA150_READ_ACCEL_XYZ			_IOWR(BMA150_IOC_MAGIC,46,short)
+#define BMA150_READ_TEMPERATURE			_IOWR(BMA150_IOC_MAGIC,47,unsigned char)
+#define BMA150_CALIBRATION				_IOWR(BMA150_IOC_MAGIC,48,short)
+#define BMA150_READ_INT					_IOWR(BMA150_IOC_MAGIC,49,unsigned char)
+static int fd;
+void event_handler(int sigum)
+{
+	int  value=8;
+	printf("bma150 got interrupt signal\n");
+	
+}
+
+int main()
+{
+	int oflags;
+    unsigned char tmp1;
+	printf("bma150 interrupt test app is running\n");
+	fd = open("/dev/bma150",O_RDWR);
+	if(fd < 0){
+	return -1;
+	}
+	signal(SIGIO,event_handler);
+	fcntl(fd,F_SETOWN,getpid());
+	oflags = fcntl(fd, F_GETFL);
+	fcntl(fd,F_SETFL, oflags|FASYNC);
+
+    tmp1=1;
+	ioctl(fd,BMA150_SET_LOW_G_INT,&tmp1);
+    ioctl(fd,BMA150_SET_ANY_MOTION_INT,&tmp1);
+    ioctl(fd,BMA150_SET_HIGH_G_INT,&tmp1);
+
+
+	while(1)
+	{
+usleep(1000);
+	}
+	return 0;
+
+}
+
